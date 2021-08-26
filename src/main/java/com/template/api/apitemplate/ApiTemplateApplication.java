e1f1d7afe3a003057c415da525296049d8fe082a
rlirette <71311174+rlirette@users.noreply.github.com>
2021-08-26 21:19:48 +0200

    Initial commit

---
+++ src/main/java/com/template/api/apitemplate/ApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.template.api.apitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTemplateApplication.class, args);
+	}
+
+}
