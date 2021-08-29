c7897ebebb49f0d93c9b952daa4606e65cd8a95e
rlirette <71311174+rlirette@users.noreply.github.com>
2021-08-30 01:00:54 +0200

    [FIX] Security UT

--- src/test/java/com/template/api/apitemplate/api/config/JwtSecurityConfigTest.java
+++ src/test/java/com/template/api/apitemplate/api/config/JwtSecurityConfigTest.java
@@ -3,26 +3,37 @@ package com.template.api.apitemplate.api.config;
 import com.template.api.apitemplate.api.model.dto.response.PlayerResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class JwtSecurityConfigTest {
 
     private TestRestTemplate restTemplate;
-    private static final String apiUrl = "http://localhost:8080/api-template/playercard/byname?name=paul";
+
+    URL base;
+    @LocalServerPort
+    int port;
 
     @BeforeEach
-    public void init() {
+    public void init() throws MalformedURLException {
         restTemplate = new TestRestTemplate("user", "password");
+        base = new URL("http://localhost:" + port + "/api-template/playercard/byname?name=paul");
     }
 
     @Test
     public void whenLoggedUserRequestsHomePage_ThenSuccess() throws IllegalStateException {
         ResponseEntity<PlayerResponse> response =
-                restTemplate.getForEntity(apiUrl, PlayerResponse.class);
+                restTemplate.getForEntity(base.toString(), PlayerResponse.class);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
@@ -31,7 +42,7 @@ class JwtSecurityConfigTest {
     public void whenUserWithWrongCredentials_thenUnauthorizedPage() {
         restTemplate = new TestRestTemplate("user", "wrongpassword");
         ResponseEntity<PlayerResponse> response =
-                restTemplate.getForEntity(apiUrl, PlayerResponse.class);
+                restTemplate.getForEntity(base.toString(), PlayerResponse.class);
 
         assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     }
